7a4b1190ec9df88d5bf99dad9818d10ffa709c80
Jack Deng <jackdeng@gmail.com>
2020-12-29 17:24:29 -0800

    cleanup

--- jbig2dec.cc
+++ jbig2dec.cc
@@ -17,44 +17,60 @@
 #include "pdfium/core/fxcodec/jbig2/JBig2_TrdProc.cpp"
 #include "pdfium/core/fxcodec/jbig2/jbig2_decoder.cpp"
 
-#include "pdfium/core/fxcodec/fx_codec.cpp"
 #include "pdfium/core/fxcodec/scanlinedecoder.cpp"
 #include "pdfium/core/fxcodec/fax/faxmodule.cpp"
 
-namespace internal
+namespace pdfium
 {
-  void *AllocOrDie(size_t num_members, size_t member_size)
+  namespace internal
   {
-    void *result = malloc(num_members * member_size);
-    if (!result)
-      abort(); // Never returns.
+    void *AllocOrDie(size_t num_members, size_t member_size)
+    {
+      void *result = malloc(num_members * member_size);
+      if (!result)
+        abort(); // Never returns.
 
-    return result;
+      return result;
+    }
+
+    void *CallocOrDie(size_t num_members, size_t member_size)
+    {
+      void *result = calloc(num_members, member_size);
+      if (!result)
+        abort(); // Never returns.
+
+      return result;
+    }
+
+    void *CallocOrDie2D(size_t w, size_t h, size_t member_size)
+    {
+      return CallocOrDie(w * h, member_size);
+    }
+
+    void *ReallocOrDie(void *ptr, size_t num_members, size_t member_size)
+    {
+      void *result = realloc(ptr, num_members * member_size);
+      if (!result)
+        abort(); // Never returns.
+
+      return result;
+    }
+  } // namespace internal
+} // namespace pdfium
+
+namespace fxcodec
+{
+  FX_SAFE_UINT32 CalculatePitch32(int bpp, int width)
+  {
+    FX_SAFE_UINT32 pitch = bpp;
+    pitch *= width;
+    pitch += 31;
+    pitch /= 32; // quantized to number of 32-bit words.
+    pitch *= 4;  // and then back to bytes, (not just /8 in one step).
+    return pitch;
   }
 
-  void *CallocOrDie(size_t num_members, size_t member_size)
-  {
-    void *result = calloc(num_members, member_size);
-    if (!result)
-      abort(); // Never returns.
-
-    return result;
-  }
-
-  void *CallocOrDie2D(size_t w, size_t h, size_t member_size)
-  {
-    return CallocOrDie(w * h, member_size);
-  }
-
-  void *ReallocOrDie(void *ptr, size_t num_members, size_t member_size)
-  {
-    void *result = realloc(ptr, num_members * member_size);
-    if (!result)
-      abort(); // Never returns.
-
-    return result;
-  }
-} // namespace internal
+} // namespace fxcodec
 
 void FX_Free(void *ptr)
 {
@@ -64,11 +80,10 @@ void FX_Free(void *ptr)
 extern "C" int jbig2Decode(int width, int height, uint8_t *data, size_t sz, uint8_t *buf)
 {
   int pitch = (width + 31) / 32 * 4;
-  std::unique_ptr<JBig2_DocumentContext> dctx{};
   auto ctx = std::make_unique<Jbig2Context>();
-  
+
   auto status = Jbig2Decoder::StartDecode(
-      ctx.get(), &dctx, width, height,
+      ctx.get(), nullptr, width, height,
       pdfium::span<uint8_t>{data, sz}, 0, pdfium::span<uint8_t>{}, 0,
       buf, pitch, nullptr);
   if (status != FXCODEC_STATUS_DECODE_FINISH)
